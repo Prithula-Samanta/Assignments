9f7e46a74b219f56f5f0b17c42aa971c5c2bf278
Prithula-Samanta <111556690+Prithula-Samanta@users.noreply.github.com>
2022-10-17 00:19:04 +0530

    Add files via upload

---
+++ Introduction To Java_Activity_1 (2)/MyProgram.java
@@ -0,0 +1,20 @@
+package com.Assignments;
+
+
+//Class  myProgram {
+//	  Public static void main(String arg[]);{
+//	  system.out.Println("My First Java Program");
+//	 }
+//	} 
+
+//The above program will no run compile the error will occer.
+
+
+
+//Now the following program execute properly
+
+class  MyProgram {
+	  public static void main(String args[]){
+	  System.out.println("My First Java Program");
+	 }
+	} 
