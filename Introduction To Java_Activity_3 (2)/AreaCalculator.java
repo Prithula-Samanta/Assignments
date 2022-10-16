492e083345de4e57870c8b194ce48536231c4058
Prithula-Samanta <111556690+Prithula-Samanta@users.noreply.github.com>
2022-10-17 00:08:42 +0530

    Add files via upload

---
+++ Introduction To Java_Activity_3 (2)/AreaCalculator.java
@@ -0,0 +1,11 @@
+package com.cognizant.shapes;
+
+public class AreaCalculator {
+  public static void main(String[] args) {
+	  Rectangle area = new Rectangle();
+	  area.leangth=12;
+	  area.breadth=8;
+	  System.out.println("The area of rectangle is " +area.calculateArea());
+	  
+}
+}
---
+++ Introduction To Java_Activity_3 (2)/Rectangle.java
@@ -0,0 +1,8 @@
+package com.cognizant.shapes;
+public class Rectangle {
+	double leangth;
+	double breadth;
+   public double calculateArea() {
+	   return leangth*breadth;
+   }
+}
